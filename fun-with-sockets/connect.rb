0ad3b20bc785324246fc546b7697518bd55aebc3
Natan Siqueira <natan.siqueira@icloud.com>
2023-08-15 12:53:36 -0300

    http call with socket

--- fun-with-sockets/connect.rb
+++ fun-with-sockets/connect.rb
@@ -1,15 +1,20 @@
 # underlying operating system socket implementation
 require 'socket'
+include Socket::Constants
 
 # creates our socket
-s = Socket.new Socket::AF_INET6, Socket::SOCK_STREAM
+socket = Socket.new AF_INET6, SOCK_STREAM
 
-# example.com ip
-addr = Socket.pack_sockaddr_in 80, '93.184.216.34'
+# example.com
+# translate port and host to as an sockaddr string
+socket_addr = Socket.pack_sockaddr_in 80, 'example.com'
 
 # connect to example.com
-s.connect addr
+socket.connect socket_addr
 
-socket.write( 'GET / HTTP/1.1\r\n\r\n' )
-
-puts socket.read
+# write a valid HTTP request
+# GET / HTTP/1.1
+# Host: example.com
+socket.send "GET / HTTP/1.1\r\nHost: example.com\r\n\r\n", 0
+results = socket.recv 4096
+puts results
