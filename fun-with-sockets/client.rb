7931fe9e8edb81aaefe26d5c79cbb7fc4b88fcfa
Natan Siqueira <natan.siqueira@icloud.com>
2023-08-15 19:21:27 -0300

    a simple tcp server

--- fun-with-sockets/client.rb
+++ fun-with-sockets/client.rb
@@ -3,11 +3,11 @@ require 'socket'
 include Socket::Constants
 
 # creates our socket
-socket = Socket.new AF_INET6, SOCK_STREAM
+socket = Socket.new AF_INET, SOCK_STREAM
 
 # example.com
 # translate port and host to as an sockaddr string
-socket_addr = Socket.pack_sockaddr_in 80, 'example.com'
+socket_addr = Socket.pack_sockaddr_in 8080, 'localhost'
 
 # connect to example.com
 socket.connect socket_addr
@@ -15,6 +15,6 @@ socket.connect socket_addr
 # write a valid HTTP request
 # GET / HTTP/1.1
 # Host: example.com
-socket.send "GET / HTTP/1.1\r\nHost: example.com\r\n\r\n", 0
+socket.send "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n", 0
 results = socket.recv 4096
 puts results
--- fun-with-sockets/server.rb
+++ fun-with-sockets/server.rb
@@ -1,3 +1,31 @@
 require 'socket'
 
-s = Socket.new 
+# create a socket c-like 
+socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+
+# pack a port and a host in sockaddr string format
+# but on server we use our local machine so localhost
+# the port can be any available port
+socket_addr = Socket.pack_sockaddr_in 8080, '127.0.0.1'
+
+# now we bind or address to our socket
+socket.bind socket_addr
+
+# 10 here is a stack size of connections that our machine
+# can store to be processed
+socket.listen(10)
+
+response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nhello world\r\n"
+
+# keep accepting connections
+loop do
+  # wait for a connections
+  client_socket, addr = socket.accept()
+
+  puts client_socket.recv 4096
+  client_socket.write response
+  client_socket.close
+end
+
+# close server the socket
+socket.close
