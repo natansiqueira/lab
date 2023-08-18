5f39fb07d76945af2ce95fa3dd8a402c58556e4e
Natan Siqueira <natan.siqueira@icloud.com>
2023-08-18 15:50:21 -0300

    add test script

--- fun-with-sockets/server.rb
+++ fun-with-sockets/server.rb
@@ -1,30 +1,33 @@
 require 'socket'
 
-# create a socket c-like 
+# create a socket c-like
 socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
 
 # pack a port and a host in sockaddr string format
 # but on server we use our local machine so localhost
 # the port can be any available port
-socket_addr = Socket.pack_sockaddr_in 8080, '127.0.0.1'
+socket_addrinfo = Socket.pack_sockaddr_in 8080, '127.0.0.1'
 
 # now we bind or address to our socket
-socket.bind socket_addr
+socket.bind socket_addrinfo
 
 # 10 here is a stack size of connections that our machine
 # can store to be processed
 socket.listen(10)
 
-response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nhello world\r\n"
+def response(request)
+  name = request.split("\r\n\r\n").last
+  "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nhello #{name}\r\n"
+end
 
 # keep accepting connections
 loop do
   # wait for a connections
-  client_socket, addr = socket.accept()
-
-  puts client_socket.recv 4096
-  client_socket.write response
-  client_socket.close
+  Thread.start(socket.accept) do |client_socket, client_addrinfo|
+    request = client_socket.recv 4096
+    client_socket.write response(request)
+    client_socket.close
+  end
 end
 
 # close server the socket
---
+++ fun-with-sockets/test_server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+for name in eeny meeny miny moe marjorie
+do
+  http POST 127.0.0.1:8080 --raw=$name
+done
+wait
+
+
