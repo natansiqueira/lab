597e605382c02a4dc3a2d6d505ca6f8e38f93555
Natan Siqueira <natan.siqueira@icloud.com>
2023-09-17 00:59:50 -0300

    add bash todo

---
+++ bash/todo/todo.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# consts
+TODO_FILE=".todos"
+
+# utils
+_have() {
+	type "$1" &>/dev/null
+}
+
+# dependencies
+! _have fzf && echo "→ fzf required" && exit 1
+
+new() {
+	touch TODO_FILE
+	echo "[ ] - $1" >>$TODO_FILE
+}
+
+list() {
+	if [ "$1" = "all" ]; then
+		cat $TODO_FILE
+	else
+		cat $TODO_FILE | grep "→"
+	fi
+}
+
+complete() {
+	task=$(cat $TODO_FILE | fzf)
+	replacement=$(echo "$task" | gsed 's/→/✓/')
+	gsed -i "s/$task/$replacement/" "$TODO_FILE"
+}
+
+help() {
+	echo "todo - simple todo"
+	echo
+	echo "Syntax: zet -[n|new|l|list|c|complete|h]"
+	echo "options:"
+	echo "n | new [title] new todo entry"
+	echo "l | list        list todos"
+	echo "          all   list all"
+	echo "h | help        print this help"
+	echo
+}
+
+case "$1" in
+n | new)
+	new "$2"
+	exit
+	;;
+l | list)
+	list "$2"
+	exit
+	;;
+c | complete)
+	complete
+	exit
+	;;
+*)
+	help
+	exit
+	;;
+esac
