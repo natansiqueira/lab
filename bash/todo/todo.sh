ce463d9c7373038e8278da8664a850dae5ea252c
Natan Siqueira <natan.siqueira@icloud.com>
2023-09-17 14:34:18 -0300

    improve todo

--- bash/todo/todo.sh
+++ bash/todo/todo.sh
@@ -14,22 +14,37 @@ _have() {
 ! [[ -d $TODO_DIR ]] && mkdir -p $TODO_DIR && touch $TODO_FILE && echo "→ todo list started ($TODO_FILE)"
 
 new() {
-	echo "→ $*" >>$TODO_FILE
+  	echo "→ $*" >>$TODO_FILE
+	echo "✓ task added"
 }
 
 list() {
 	if [ "$1" = "all" ]; then
+		[[ $(cat $TODO_FILE | wc -l | xargs) -eq 0 ]] && echo '→ no entries' && exit 0
 		cat $TODO_FILE
+		echo "✓ total items $(cat $TODO_FILE | wc -l | xargs)"
 	else
-		[[ $(cat $TODO_FILE | grep "→" | wc -l | xargs) -eq 0 ]] && echo '→ no entries' && exit 0
+		tasks=$(cat $TODO_FILE | grep "→" | wc -l | xargs)
+		[[ $tasks -eq 0 ]] && echo '→ no entries' && exit 0
 		cat $TODO_FILE | grep "→"
+		echo "✓ total items $tasks"
 	fi
 }
 
-complete() {
-	task=$(cat $TODO_FILE | fzf)
+completed() {
+	task=$(cat $TODO_FILE | fzf --height=15 --border --reverse -e -i --tiebreak=begin --prompt='Task: ')
+	! [[ -n $task ]] && echo '→ operation cancelled' && exit 1
 	replacement=$(echo "$task" | gsed 's/→/✓/')
 	gsed -i "s/$task/$replacement/" "$TODO_FILE"
+	echo "✓ $task mark as completed"
+}
+
+delete() {
+	[[ $(cat $TODO_FILE | wc -l | xargs) -eq 0 ]] && echo '→ no entries' && exit 0
+	task=$(cat $TODO_FILE | fzf --height=15 --border --reverse -e -i --tiebreak=begin --prompt='Task: ')
+	! [[ -n $task ]] && echo '→ operation cancelled' && exit 1
+	gsed -i "/$task/d" "$TODO_FILE"
+	echo "✓ task deleted"
 }
 
 help() {
@@ -37,29 +52,34 @@ help() {
 	echo
 	echo "Syntax: zet -[n|new|l|list|c|completed|h]"
 	echo "options:"
-	echo "n | new [title] new todo entry"
-	echo "l | list        list active tasks"
-	echo "          all   list tasks including completed"
-	echo "c | completed   mark todo as completed"
-	echo "h | help        print this help"
+	echo "n | new [title]	new todo entry"
+	echo "l | list			list active tasks"
+	echo "			all		list tasks including completed"
+  	echo "c | comp			mark todo as completed"
+	echo "d | del			delete a task"
+	echo "h | help        	print this help"
 	echo
 }
 
 case "$1" in
-n | new)
-	new "${@:2}"
-	exit
-	;;
-l | list)
-	list "$2"
-	exit
-	;;
-c | complete)
-	complete
-	exit
-	;;
-*)
-	help
-	exit
-	;;
+	n | new)
+    	new "${@:2}"
+    	exit
+    ;;
+  	l | list)
+    	list "$2"
+    	exit
+    ;;
+  	c | comp)
+    	completed
+    	exit
+    ;;
+	d | del)
+    	delete
+    	exit
+    ;;
+ 	*)
+    	help
+    	exit
+    ;;
 esac
